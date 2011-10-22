fcc79575f29636cfdd02c15fc407f26355276ddb
rodolfodelimaviana@gmail.com <rodolfodelimaviana@gmail.com@8c9fc829-2cc4-6f44-4c2a-3f0fdc94196d>
2011-10-22 14:32:12 +0000

    Com apenas um erro

--- trunk/SIMilestone2/src/Classes/AtividadesUsuario.java
+++ trunk/SIMilestone2/src/Classes/AtividadesUsuario.java
@@ -2,107 +2,86 @@ package Classes;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 
 public class AtividadesUsuario {
-	
+
 	List<Atividade> listaAtividades;
 	List<Long> dataCriacaoAtividades;
-	
-	public AtividadesUsuario(){
+
+	public AtividadesUsuario() {
 		listaAtividades = new ArrayList<Atividade>();
 		dataCriacaoAtividades = new ArrayList<Long>();
 	}
 
-	public void adicionarAtividades(Atividade atividade){
-		if(!listaAtividades.contains(atividade) && !atividadeJaExiste(atividade)){
+	public void adicionarAtividades(Atividade atividade) {
+		if (!listaAtividades.contains(atividade)
+				&& !atividadeJaExiste(atividade)) {
 			System.out.println((atividade.getAtividade()));
 			listaAtividades.add(atividade);
 			Collections.sort(listaAtividades);
-		    try {  
-		        Thread.sleep(10);  
-		     } catch (Exception e) {  
-		        e.printStackTrace();  
-		     }  
+			try {
+				Thread.sleep(10);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
-	
-	public String getCriacaoAtividades(){
+
+	public String getCriacaoAtividades() {
 		return formataArray2(dataCriacaoAtividades).toString();
 	}
-	
-	public List<Atividade> getAtividades(){
+
+	public List<Atividade> getAtividades() {
 		return (listaAtividades);
 	}
-	
-	public boolean naoTemAtividades(){
+
+	public boolean naoTemAtividades() {
 		return listaAtividades.isEmpty();
 	}
-	
+
 	public static void main(String[] args) {
-	
-		
-		//String x = "[Steven Paul Jobs e Mark Zuckerberg são amigos agora][William Henry Gates III e Mark Zuckerberg são amigos agora][Mark Zuckerberg e William Henry Gates III são amigos agora, Mark Zuckerberg e Steven Paul Jobs são amigos agora]";
-		
+
+		// String x =
+		// "[Steven Paul Jobs e Mark Zuckerberg são amigos agora][William Henry Gates III e Mark Zuckerberg são amigos agora][Mark Zuckerberg e William Henry Gates III são amigos agora, Mark Zuckerberg e Steven Paul Jobs são amigos agora]";
+
 		String x = "Mark Zuckerberg e William Henry Gates III são amigos agora;Mark Zuckerberg e Steven Paul Jobs são amigos agora;";
 
 		String z = "";
-		int cont  = 0;
-		for(String i : x.split(";")){
-			if(cont!=x.split(";").length-1){
-				z+=i+"; ";
-			}
-			else{
-				z+=i;
+		int cont = 0;
+		for (String i : x.split(";")) {
+			if (cont != x.split(";").length - 1) {
+				z += i + "; ";
+			} else {
+				z += i;
 			}
 			cont++;
-		
+
 		}
 		System.out.println(z);
 
 	}
-	
-	
-	private ArrayList<String> formataArray(List<String> lista){
-		
-		ArrayList<String> temp = new ArrayList<String>();
-		
-		for(int i = lista.size()-1; i>=0;i--){
-			temp.add(lista.get(i));
-		}
-		return temp;
-		
-	}
-	
-private ArrayList<Long> formataArray2(List<Long> lista){
-		
+
+	private ArrayList<Long> formataArray2(List<Long> lista) {
+
 		ArrayList<Long> temp = new ArrayList<Long>();
-		
-		for(int i = lista.size()-1; i>=0;i--){
+
+		for (int i = lista.size() - 1; i >= 0; i--) {
 			temp.add(lista.get(i));
 		}
 		return temp;
-		
+
 	}
 
-
-private boolean atividadeJaExiste(Atividade atv){
-	System.out.println(atv.getAtividade());
-	for(Atividade atvs: listaAtividades){
-		if(atvs.getAtividade().trim().equals(atv.getAtividade().trim())){
-			return true;
-		}
-	}return false;
-}
-	private void elimina(){
-		List<Atividade> x = new ArrayList<Atividade>();
-		
-		for(Atividade i: listaAtividades){
-			if(!atividadeJaExiste(i)){
-				x.add(i);
+	private boolean atividadeJaExiste(Atividade atv) {
+		System.out.println(atv.getAtividade());
+		for (Atividade atvs : listaAtividades) {
+			if (atvs.getAtividade().trim().equals(atv.getAtividade().trim())) {
+				return true;
 			}
-		}listaAtividades = x;
+		}
+		return false;
 	}
+
 
 }
